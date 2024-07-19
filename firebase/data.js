21f484b525525ea47a76a9107442e71b04dc0a5b
maksim-pokhiliy <maksim.pokhiliy@gmail.com>
2024-07-19 22:16:28 +0300

    removed sensitive data

---
+++ firebase/data.js
@@ -0,0 +1,125 @@
+export const PRODUCTS = {
+  TSHIRTS: {
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/ukrainian-tactical-gear.appspot.com/o/BLACK.jpg?alt=media&token=2561e97d-4516-4b38-8fbc-65d75afc82cd",
+    title: "T-Shirts",
+    products: [
+      {
+        title: "«Death» Black",
+        description:
+          "Small print on the left chest: Ukrainian Tactical Gear logo. Back print with 'With you or for you it depends on how you trained' slogan и large graphic.",
+        image:
+          "https://firebasestorage.googleapis.com/v0/b/ukrainian-tactical-gear.appspot.com/o/BLACK.jpg?alt=media&token=2561e97d-4516-4b38-8fbc-65d75afc82cd",
+        sizes: ["M", "L", "XL", "2XL"],
+        price: 1000,
+        availability: false,
+      },
+      {
+        title: "«Welcome» Black",
+        description:
+          "Small print on the left chest: Ukrainian Tactical Gear logo. Back print with 'Welcome to Ukraine, suka!' slogan и large graphic.",
+        image:
+          "https://firebasestorage.googleapis.com/v0/b/ukrainian-tactical-gear.appspot.com/o/BLACK1.jpg?alt=media&token=2561e97d-4516-4b38-8fbc-65d75afc82cd",
+        sizes: ["M", "L", "XL", "2XL"],
+        price: 1000,
+        availability: false,
+      },
+      {
+        title: "«Death» Green",
+        description:
+          "Small print on the left chest: Ukrainian Tactical Gear logo. Back print with 'With you or for you it depends on how you trained' slogan и large graphic.",
+        image:
+          "https://firebasestorage.googleapis.com/v0/b/ukrainian-tactical-gear.appspot.com/o/GREEN.jpg?alt=media&token=2561e97d-4516-4b38-8fbc-65d75afc82cd",
+        sizes: ["M", "L", "XL", "2XL"],
+        price: 1000,
+        availability: false,
+      },
+      {
+        title: "«Welcome» Green",
+        description:
+          "Small print on the left chest: Ukrainian Tactical Gear logo. Back print with 'Welcome to Ukraine, suka!' slogan и large graphic.",
+        image:
+          "https://firebasestorage.googleapis.com/v0/b/ukrainian-tactical-gear.appspot.com/o/GREEN1.jpg?alt=media&token=2561e97d-4516-4b38-8fbc-65d75afc82cd",
+        sizes: ["M", "L", "XL", "2XL"],
+        price: 1000,
+        availability: false,
+      },
+      {
+        title: "«Death» Grey",
+        description:
+          "Small print on the left chest: Ukrainian Tactical Gear logo. Back print with 'With you or for you it depends on how you trained' slogan и large graphic.",
+        image:
+          "https://firebasestorage.googleapis.com/v0/b/ukrainian-tactical-gear.appspot.com/o/GREY.jpg?alt=media&token=2561e97d-4516-4b38-8fbc-65d75afc82cd",
+        sizes: ["M", "L", "XL", "2XL"],
+        price: 1000,
+        availability: false,
+      },
+      {
+        title: "«Welcome» Grey",
+        description:
+          "Small print on the left chest: Ukrainian Tactical Gear logo. Back print with 'Welcome to Ukraine, suka!' slogan и large graphic.",
+        image:
+          "https://firebasestorage.googleapis.com/v0/b/ukrainian-tactical-gear.appspot.com/o/GREY1.jpg?alt=media&token=2561e97d-4516-4b38-8fbc-65d75afc82cd",
+        sizes: ["M", "L", "XL", "2XL"],
+        price: 1000,
+        availability: false,
+      },
+    ],
+  },
+  PATCHES: {
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/ukrainian-tactical-gear.appspot.com/o/patches_utg.jpg?alt=media&token=2561e97d-4516-4b38-8fbc-65d75afc82cd",
+    title: "Patches",
+    products: [
+      {
+        title: "«Waiting»",
+        image:
+          "https://firebasestorage.googleapis.com/v0/b/ukrainian-tactical-gear.appspot.com/o/patches_waiting.jpg?alt=media&token=2561e97d-4516-4b38-8fbc-65d75afc82cd",
+        price: 300,
+        availability: true,
+      },
+      {
+        title: "«Welcome»",
+        image:
+          "https://firebasestorage.googleapis.com/v0/b/ukrainian-tactical-gear.appspot.com/o/patches_welcome.jpg?alt=media&token=2561e97d-4516-4b38-8fbc-65d75afc82cd",
+        price: 300,
+        availability: true,
+      },
+      {
+        title: "«Death»",
+        image:
+          "https://firebasestorage.googleapis.com/v0/b/ukrainian-tactical-gear.appspot.com/o/patches_with_you.jpg?alt=media&token=2561e97d-4516-4b38-8fbc-65d75afc82cd",
+        price: 300,
+        availability: true,
+      },
+      {
+        title: "«UTG»",
+        image:
+          "https://firebasestorage.googleapis.com/v0/b/ukrainian-tactical-gear.appspot.com/o/patches_utg.jpg?alt=media&token=2561e97d-4516-4b38-8fbc-65d75afc82cd",
+        price: 300,
+        availability: true,
+      },
+      {
+        title: "Set of «Waiting, Welcome, Death»",
+        image:
+          "https://firebasestorage.googleapis.com/v0/b/ukrainian-tactical-gear.appspot.com/o/patches_set.jpg?alt=media&token=2561e97d-4516-4b38-8fbc-65d75afc82cd",
+        price: 800,
+        availability: true,
+      },
+    ],
+  },
+  STICKERS: {
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/ukrainian-tactical-gear.appspot.com/o/stickers2.JPG?alt=media&token=2561e97d-4516-4b38-8fbc-65d75afc82cd",
+    title: "Stickers",
+    products: [
+      {
+        title: "«Sticker Pack»",
+        image:
+          "https://firebasestorage.googleapis.com/v0/b/ukrainian-tactical-gear.appspot.com/o/stickers2.JPG?alt=media&token=2561e97d-4516-4b38-8fbc-65d75afc82cd",
+        price: 250,
+        availability: true,
+      },
+    ],
+  },
+};
